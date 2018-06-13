e0d4266c57f6b188fe9995250a8431c670564959
Guillaume Massé <masgui@gmail.com>
2018-06-13 12:17:56 +0200

    change public url from /accessible-scala-web to /accessible-scala-demo

--- publish.sh
+++ publish.sh
@@ -3,4 +3,4 @@
 rm -rf index.html
 rm -rf FiraCode*
 rm -rf app.*
-cp -R ~/accessible-scala/web/target/scala-2.12/scalajs-bundler/main/out/accessible-scala-web/* .
+cp -R ~/accessible-scala/web/target/scala-2.12/scalajs-bundler/main/out/accessible-scala-demo/* .
